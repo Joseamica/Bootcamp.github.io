aaf6b1219ddc81daf4e6f9ab68f801e0054ec7d1
Joseamica <jo_amieva7@hotmail.com>
2019-06-20 21:14:36 -0500

    Added new exercices and homework 4 not finished

---
+++ Pandas/Clase_1/Exercices/netflix/netflix.py
@@ -0,0 +1,30 @@
+# Modules
+import os
+import csv
+
+# Prompt user for video lookup
+video = input("What show or movie are you looking for? ")
+
+# Set path for file"
+csvpath = os.path.join("netflix_ratings.csv")
+
+# Bonus
+# ------------------------------------------
+# Set variable to check if we found the video
+found = False
+
+# Open the CSV
+with open(csvpath, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Loop through looking for the video
+    for row in csvreader:
+        if row[0] == video:
+            print(row[0] + " is rated " + row[1] + " with a rating of " + row[6])
+
+            # Set variable to confirm we have found the video
+            found = True
+
+    # If the video is never found, alert the user
+    if found is False:
+        print("Sorry about this, we don't seem to have what you are looking for!")
