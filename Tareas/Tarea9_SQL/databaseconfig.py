8ec65b57398ee22bb88d2544eeb600f00fd49d89
Joseamica <jo_amieva7@hotmail.com>
2019-07-29 21:08:11 -0500

    added sql homework

---
+++ Tareas/Tarea9_SQL/databaseconfig.py
@@ -0,0 +1,7 @@
+import preprocessing
+mysql = {'host': 'localhost',
+         'user': 'postgres',
+         'passwd': 'aloha1993',
+         'db': 'homework_db'}
+
+use_anonymous = True
---
+++ Week 9 - SQL/Clase_1/Week 9 - SQL_Activities_1_Activities_08-Stu_CRUD_Resources_schema.sql
@@ -0,0 +1,23 @@
+-- Drop table if exists
+DROP TABLE firepower;
+
+-- Create new table to import data
+CREATE TABLE firepower (
+	country VARCHAR,
+	ISO3 VARCHAR,
+	rank INT,
+	TotalPopulation INT,
+	ManpowerAvailable INT,
+	TotalMilitaryPersonnel INT,
+	ActivePersonnel INT,
+	ReservePersonnel INT,
+	TotalAircraftStrength INT,
+	FighterAircraft INT,
+	AttackAircraft INT,
+	TotalHelicopterStrength INT,
+	AttackHelicopters INT
+);
+
+-- Import data from firepower.csv
+-- View the table to ensure all data has been imported correctly
+SELECT * FROM firepower;
